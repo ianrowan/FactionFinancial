6bf1781077318f2fbfb1485ccb046ae80475c17e
ianrowan <i.rowan299@gmail.com>
2022-05-03 14:35:04 -0400

    Add auto LP to StakeAura,remote balance check to ERC20

--- contracts/AuraToken.sol
+++ contracts/AuraToken.sol
@@ -3,14 +3,34 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract AuraToken is ERC20, ERC20Burnable {
+  using Address for address;
 
   address private owner;
+  address[] private otherContracts;
 
   constructor(string memory symbol, string memory name) ERC20(name, symbol){
     owner = msg.sender;
     _mint(msg.sender, 10000000);
+  }
+
+  //Allows operators to get Users total balance + balance in remote contracts(staking, etc)
+  function remoteBalanceOf(address user) public returns(uint){
+    uint balance = balanceOf(user);
+    for(uint i = 0; i < otherContracts.length; i++){
+       bytes memory result = otherContracts[i].functionCall(
+        abi.encodeWithSignature("balanceOf(address)", user)
+      );
+      balance += abi.decode(result, (uint));
+    }
+    return balance;
+  }
+
+  function addBalanceContract(address contr) public isOwner {
+    require(contr.isContract());
+    otherContracts.push(contr);
   }
 
   function mint(uint total) public isOwner{
--- contracts/StakeAura.sol
+++ contracts/StakeAura.sol
@@ -3,10 +3,15 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 
 contract AuraStaking is PaymentSplitter{
 
     IERC20 public stakingToken;
+    
+    address private constant ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
 
     address private owner;
     uint public rewardRate;
@@ -20,9 +25,10 @@ contract AuraStaking is PaymentSplitter{
     mapping(address => uint) private _lastUpdateTime;
 
     /*
-      wallets[0] = rewardpool
-      wallets[1] = LPool
-      wallets[2] = treasury
+                              shares/10
+      wallets[0] = rewardpool ex 4
+      wallets[1] = tresury ex 4
+      remainder share = liq pool 2
     */
     constructor(address _stakingToken, uint _rewardRate, address[] memory wallets, uint256[] memory walletShares)
     PaymentSplitter(wallets, walletShares) {
@@ -54,7 +60,10 @@ contract AuraStaking is PaymentSplitter{
       _nodeCount[msg.sender] += _amount/10;
       bondCount += _amount/10;
 
+      //Release to shares of reward pool + tresury
       releaseAll(stakingToken);
+      //Add remaining tokens to LP
+      _addLiquidity();
     }
 
     function withdraw(uint _amount) external updateReward(msg.sender) isBond(_amount){
@@ -65,8 +74,8 @@ contract AuraStaking is PaymentSplitter{
       _nodeCount[msg.sender] -= _amount/10;
       bondCount -= _amount/10;
 
-      stakingToken.transferFrom(payee(0), msg.sender, _amount * shares(payee(0))/9);
-      stakingToken.transferFrom(payee(2), msg.sender, _amount * shares(payee(2))/9);
+      stakingToken.transferFrom(payee(0), msg.sender, _amount * shares(payee(0)));
+      stakingToken.transferFrom(payee(2), msg.sender, _amount * shares(payee(2)));
     }
 
     function getReward() external updateReward(msg.sender) {
@@ -75,7 +84,73 @@ contract AuraStaking is PaymentSplitter{
         stakingToken.transferFrom(payee(0), msg.sender, reward);
     }
 
-    function getStake(address addr) external view returns(uint){
+    function _addLiquidity() internal {
+      
+      uint balance = stakingToken.balanceOf(address(this));
+      IUniswapV2Router02 UniswapV2Router02 = IUniswapV2Router02(ROUTER);
+
+      address[] memory path = new address[](2);
+      path[0] = address(stakingToken);
+      path[1] = UniswapV2Router02.WETH();
+
+      stakingToken.approve(ROUTER, balance);
+
+      uint preBal = IERC20(path[1]).balanceOf(address(this));
+
+      UniswapV2Router02.swapExactTokensForETHSupportingFeeOnTransferTokens(
+        balance/2,
+        0,
+        path,
+        address(this),
+        block.timestamp
+        );
+
+      uint postBal = IERC20(path[1]).balanceOf(address(this));
+
+      IERC20(path[1]).approve(ROUTER, postBal);
+      UniswapV2Router02.addLiquidity(
+        path[0],
+        path[1],
+        balance/2,
+        postBal - preBal,
+        0,
+        0,
+        address(this),
+        block.timestamp
+        );
+    }
+
+    function removeLiquidity(uint shares) public isOwner {
+      IUniswapV2Router02 UniswapV2Router02 = IUniswapV2Router02(ROUTER);
+      address pair = IUniswapV2Factory(FACTORY).getPair(address(stakingToken), UniswapV2Router02.WETH());
+
+      uint liquidity = IERC20(pair).balanceOf(address(this));
+      IERC20(pair).approve(ROUTER, liquidity);
+
+      require(liquidity > shares, "Not enough liquidity shares");
+
+      (uint amountA, uint amountB) = UniswapV2Router02.removeLiquidity(
+        address(stakingToken),
+        UniswapV2Router02.WETH(),
+        shares,
+        0, 0,
+        address(this),
+        block.timestamp
+        );
+
+        IERC20(UniswapV2Router02.WETH()).transfer(msg.sender, amountB);
+        releaseAll(stakingToken);
+    }
+
+    function getLiquidityShares() public isOwner view returns(uint liquidity){
+
+      IUniswapV2Router02 UniswapV2Router02 = IUniswapV2Router02(ROUTER);
+      address pair = IUniswapV2Factory(FACTORY).getPair(address(stakingToken), UniswapV2Router02.WETH());
+
+      return IERC20(pair).balanceOf(address(this));
+    }
+
+    function balanceOf(address addr) external view returns(uint){
       return _balances[addr];
     }
     
